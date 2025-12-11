4a4a895e9e8b0c109602fa8d0a257ad051cd430f
shreeyoggaikwad-tipic <shreeyoggaikwad22@gmail.com>
2025-12-11 11:27:27 +0530

    Host Configuration

--- resources/frontend/src/utils/host.js
+++ resources/frontend/src/utils/host.js
@@ -1,6 +1,5 @@
  const host = "http://127.0.0.1:8000";
+//  const host = "";
 //  const host = "https://khalilah-unbequeathable-purposively.ngrok-free.dev";
-// const host = "https://ranveer.tipic.co.in";
-// const host = "https://ranveerrosenursery.com";
 
 export default host;
